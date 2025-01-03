9386a4da58042ace485511fccc1a3c22a98f994c
Oleh <musiychuk09@gmail.com>
2025-01-03 17:11:45 +0200

    Create DishType with name & description fields

--- kitchen/models.py
+++ kitchen/models.py
@@ -12,3 +12,8 @@ class Dish(models.Model):
     )
     cooks = models.ManyToManyField("Cook", related_name="dishes")
     ingredients = models.ManyToManyField("Ingredient", related_name="dishes")
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)

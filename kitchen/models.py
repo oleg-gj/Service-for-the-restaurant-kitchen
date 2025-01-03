85453930f09bf029edcd6e37a6f0d7e1ac186a1f
Oleh <musiychuk09@gmail.com>
2025-01-03 15:45:19 +0200

    Put in lower case Kitchen_service

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(
+        "DishType",
+        related_name="dishes",
+        on_delete=models.CASCADE
+    )
+    cooks = models.ManyToManyField("Cook", related_name="dishes")

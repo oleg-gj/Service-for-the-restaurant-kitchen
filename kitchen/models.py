f3ad89c026ec9f20f255347211c7ec6dae26f843
Oleh <musiychuk09@gmail.com>
2025-01-03 17:49:03 +0200

    Create Cook as AbstractUser

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -22,3 +23,7 @@ class DishType(models.Model):
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, unique=True)
     caloric_content = models.IntegerField(default=0)
+
+
+class Cook(AbstractUser):
+    year_of_experience = models.IntegerField(default=0)

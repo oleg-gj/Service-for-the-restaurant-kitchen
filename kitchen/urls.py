0cf05a88916acdf7f2a5969f12ac910b85e68d87
Oleh <musiychuk09@gmail.com>
2025-01-04 20:40:21 +0200

    Append path "cooks/<int:pk>/" to kitchen/urls.py

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from kitchen import views
-from kitchen.views import CookListView
+from kitchen.views import CookListView, CookDetailView
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("cooks/",CookListView.as_view(), name="cook-list"),
+    path("cooks/<int:pk>/",CookDetailView.as_view(), name="cook-detail"),
 ]
 
 app_name = "kitchen"

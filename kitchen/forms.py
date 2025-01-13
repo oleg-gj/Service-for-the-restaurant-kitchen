c8e81f92ad08cbcfc061a893b795e8668251cf88
Oleh <musiychuk09@gmail.com>
2025-01-13 14:37:37 +0200

    Create forms.py with SearchForm, CookSearchForm and DishForm

---
+++ kitchen/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+from kitchen.models import Ingredient, Dish
+
+
+class SearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"}),
+    )
+
+
+class CookSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"}),
+    )
+
+
+class DishForm(forms.ModelForm):
+    cooks = forms.ModelChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+    )
+    ingredients = forms.ModelChoiceField(
+        queryset=Ingredient.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+    )
+
+    class Meta:
+        model = Dish
+        fields = "__all__"

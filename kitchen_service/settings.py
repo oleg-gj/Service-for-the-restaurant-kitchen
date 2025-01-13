02b2f72448c1df70781a86de35323b01210ff56f
Oleh <musiychuk09@gmail.com>
2025-01-13 14:56:47 +0200

    Add LOGOUT_REDIRECT_URL, CRISPY_TEMPLATE_PACK, CRISPY_ALLOWED_TEMPLATE_PACKS. Change LOGIN_REDIRECT_URL

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -136,4 +136,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "/kitchen/"
+LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "/"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"

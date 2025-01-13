961fb61a27674a523d58a5eadd146624060925bc
Oleh <musiychuk09@gmail.com>
2025-01-13 14:49:20 +0200

    Append static for urlpatterns in urls.py

--- kitchen_service/urls.py
+++ kitchen_service/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,4 +24,4 @@ urlpatterns = [
     path("", include("kitchen.urls", namespace="kitchen")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

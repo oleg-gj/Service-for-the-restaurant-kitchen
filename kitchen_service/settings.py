3c667903a498df2a0aa40ce861bd67ce19c66811
Oleh <musiychuk09@gmail.com>
2025-01-14 09:36:28 +0200

    Add Django Debug Toolbar
    Add INTERNAL_IPS; "debug_toolbar" to INSTALLED_APPS; "debug_toolbar.middleware.DebugToolbarMiddleware" to MIDDLEWARE

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -29,6 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = ["127.0.0.1",]
+
 
 # Application definition
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "kitchen",
     "crispy_forms",
     "crispy_bootstrap5"
@@ -52,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "kitchen_service.urls"

812c5fe761783d02456a977d25455b8793436a0a
Oleh <musiychuk09@gmail.com>
2025-01-18 22:35:04 +0200

    Add default=0 for price in Dish

--- kitchen/models.py
+++ kitchen/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class Dish(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     dish_type = models.ForeignKey(
         "DishType",
         related_name="dishes",
